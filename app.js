e3268445437ae91918535da5fdb17f843b4cd2f0
hardik231 <hrkumar231@gmail.com>
2021-03-27 18:10:44 -0700

    updated package.json

--- app.js
+++ app.js
@@ -5,9 +5,11 @@ const router = require('./routes/router');
 
 const app = express();
 
+const port = process.env.PORT | 3000;
+
 app.use(express.static("public"));
 app.use(express.json())
 
 app.use("/api/articles", router)
 
-app.listen(3000, () => console.log("Server started on port 3000"))
+app.listen(3000, () => console.log("Server started on port "+port))
